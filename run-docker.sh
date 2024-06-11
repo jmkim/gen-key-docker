c6961057334d159876e9dc260863cf1e1d6dc122
Jongmin Kim <jmkim@pukyong.ac.kr>
2024-06-11 16:20:45 +0900

    Reject the execution when argument not passed
    Signed-off-by: Jongmin Kim <jmkim@pukyong.ac.kr>

--- run-docker.sh
+++ run-docker.sh
@@ -4,4 +4,10 @@ GGK_DOCKERNAME="$1"
 GGK_HOSTDIR="$(pwd)"
 GGK_WORKDIR="/root/gpg-gen-key"
 
-docker run -itd -v "$GGK_HOSTDIR":"$GGK_WORKDIR" debian:unstable "$GGK_WORKDIR/gen-key-docker.sh" --name "$GGK_DOCKERNAME"
+if [ $# -eq 0 ]
+then
+    echo "Usage: $0 [container_name]"
+    exit 1
+fi
+
+docker run -itd -v "$GGK_HOSTDIR":"$GGK_WORKDIR" --name "$GGK_DOCKERNAME" debian:unstable "$GGK_WORKDIR/gen-key-docker.sh"
